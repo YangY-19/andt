376f28ebcee2006aed01f415d4e98a322af5c189
dongyang.liu <dongyang.liu@cyberway.net.cn>
2020-07-14 17:57:54 +0800

    storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,72 @@
-import React from 'react';
-import './App.css';
-// import Button from './component/Button/button'
-// import Menu from './component/Menu/menu'
-// import SubMenu from './component/Menu/subMenu'
-// import MenuItem from './component/Menu/menuItem'
-// import { Tabs } from 'vikingship'
+import React, { useState } from "react";
+import "./App.css";
+import Button from "./component/Button/button";
+import Menu from "./component/Menu/menu";
+import SubMenu from "./component/Menu/subMenu";
+import MenuItem from "./component/Menu/menuItem";
 // import Alert from './component/Alert/alert'
-import Tabs from './component/Tabs/tabs'
-import TabItem from './component/Tabs/tabItem'
-import Icon from './component/Icon/Icon';
+import Tabs from "./component/Tabs/tabs";
+import TabItem from "./component/Tabs/tabItem";
+import Icon from "./component/Icon/Icon";
+import Transition from "./component/Transition/transition";
 const App: React.FC = () => {
-  const FUN  = (e:any) => {
-debugger
-  }
+  const [count, setCount] = useState(true);
+  const FUN = (e: any) => {
+    setCount(!count);
+  };
+
   return (
-    <div className="App" >
-       {/* <Button btnType='link' href="www.baidu.com">登录</Button>
+    <div className="App">
+      {/* <Button btnType='link' href="www.baidu.com">登录</Button>
        <Button  btnType='primary' size="lg">登录</Button>
        <Button onClick={() => {alert('1')}} >登录</Button> */}
-       {/* <div style={{width:200}}>
-       <Menu defaultIndex='0' defaultOpenSubMenus={['2']} mode="vertical" chinoiserie={true} favIcon={<img src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"></img>}>
-         <MenuItem>视频</MenuItem>
-         <MenuItem>图片</MenuItem>
-         <SubMenu title="主页">
-             <MenuItem>首页1</MenuItem>
-             <MenuItem>首页2</MenuItem>
-         </SubMenu>
-         <MenuItem>视频</MenuItem>
-         <MenuItem>图片</MenuItem>
-       </Menu>
-       </div> */}
-       {/* <Menu defaultIndex='0' onSelect={(index) => {}} >
-         <Menu.Item>视频</Menu.Item>
-         <Menu.Item>图片</Menu.Item>
-         <Menu.SubMenu title="主页">
-             <Menu.Item>首页</Menu.Item>
-             <Menu.Item>首页</Menu.Item>
-         </Menu.SubMenu>
-         <Menu.Item>视频</Menu.Item>
-         <Menu.Item>图片</Menu.Item>
-       </Menu> */}
-       {/* <Alert onClose={() => {alert('1')}} message="测试一下标题"  closable ></Alert> */}
-
-       <Tabs onTabClick={(e) => {FUN(e)}}>
-         <TabItem addIcon={ <Icon icon="coffee"></Icon>} tab="第一页">第一页类容</TabItem>
-         <TabItem tab="第二页">第二页类容</TabItem>
-       </Tabs>
-       <Icon icon="coffee"></Icon>
+      <div>
+        <Menu
+          defaultIndex="0"
+          defaultOpenSubMenus={["2"]}
+          mode="horizontal"
+          chinoiserie={false}
+          favIcon={
+            <img src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"></img>
+          }
+        >
+          <MenuItem>视频</MenuItem>
+          <MenuItem>图片</MenuItem>
+          <SubMenu title="主页">
+            <MenuItem>首页1</MenuItem>
+            <MenuItem>首页2</MenuItem>
+          </SubMenu>
+          <MenuItem>视频</MenuItem>
+          <MenuItem>图片</MenuItem>
+        </Menu>
+      </div>
+      {/* <Alert onClose={() => {alert('1')}} message="测试一下标题"  closable ></Alert> */}
+      {count ? "trure" : "false"}
+      <Transition in={count} timeout={300} animation="zoom-in-left">
+        <div>
+          {" "}
+          <Button
+            onClick={() => {
+              alert("1");
+            }}
+          >
+            登录
+          </Button>
+        </div>
+      </Transition>
+      <Tabs
+        onTabClick={(e) => {
+          FUN(e);
+        }}
+      >
+        <TabItem addIcon={<Icon icon="coffee"></Icon>} tab="第一页">
+          第一页类容
+        </TabItem>
+        <TabItem tab="第二页">第二页类容</TabItem>
+      </Tabs>
+      <Icon icon="coffee"></Icon>
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/component/Menu/subMenu.tsx
+++ src/component/Menu/subMenu.tsx
@@ -1,88 +1,106 @@
-import React, { FC, CSSProperties, Children, FunctionComponentElement, cloneElement, useContext, MouseEvent, useState } from 'react'
-import classNames from 'classnames'
-import { MenuItemProps } from './menuItem'
-import { MenuContext } from './menu'
+import React, {
+  FC,
+  CSSProperties,
+  Children,
+  FunctionComponentElement,
+  cloneElement,
+  useContext,
+  MouseEvent,
+  useState,
+} from "react";
+import classNames from "classnames";
+import { MenuItemProps } from "./menuItem";
+import { MenuContext } from "./menu";
+import Transition from "../Transition/transition";
 
 interface SubMenuProps {
-    index?: string,
-    title?: string,
-    className?: string,
-    style?: CSSProperties
+  index?: string;
+  title?: string;
+  className?: string;
+  style?: CSSProperties;
 }
-const SubMenu: FC<SubMenuProps> = props => {
-    const { index, children, className, title, style } = props
-    const context = useContext(MenuContext)
-    const classes = classNames('menu-item submenu-item', className, {
-        'is-active': context.index === index,
-        'menu-item-chinoiserie': context.menuType
-    })
+const SubMenu: FC<SubMenuProps> = (props) => {
+  const { index, children, className, title, style } = props;
+  const context = useContext(MenuContext);
+  const classes = classNames("menu-item submenu-item", className, {
+    "is-active": context.index === index,
+    "menu-item-chinoiserie": context.menuType,
+  });
 
-    const openedSubMenus = context.defaultOpenSubMenus as Array<string>
-    const isOpened:boolean = (index && context.mode === "vertical") ? openedSubMenus.includes(index) : false
-    const [menuOpen, setOpen] = useState(isOpened)
-    const handleClick = (e: MouseEvent) => {
-        e.preventDefault()
-        setOpen(!menuOpen)
-    }
+  const openedSubMenus = context.defaultOpenSubMenus as Array<string>;
+  const isOpened: boolean =
+    index && context.mode === "vertical"
+      ? openedSubMenus.includes(index)
+      : false;
+  const [menuOpen, setOpen] = useState(isOpened);
+  const handleClick = (e: MouseEvent) => {
+    e.preventDefault();
+    setOpen(!menuOpen);
+  };
 
-    const titleClasses = classNames('submenu-title', {
-       "title-active": menuOpen
-    })
+  const titleClasses = classNames("submenu-title", {
+    "title-active": menuOpen,
+  });
 
-    const clickEvents = context.mode === 'vertical' ? {
-        onClick: handleClick
-    } : ''
+  const clickEvents =
+    context.mode === "vertical"
+      ? {
+          onClick: handleClick,
+        }
+      : "";
 
-    let timer: any
-    const handleMouse = (e:MouseEvent, toggle: boolean) => {
-         clearTimeout(timer)
-         e.preventDefault()
-         timer = setTimeout(() => {
-            setOpen(toggle)
-         }, 300)
-    }
+  let timer: any;
+  const handleMouse = (e: MouseEvent, toggle: boolean) => {
+    clearTimeout(timer);
+    e.preventDefault();
+    timer = setTimeout(() => {
+      setOpen(toggle);
+    }, 300);
+  };
 
+  const hoverEvents =
+    context.mode !== "vertical"
+      ? {
+          onMouseEnter: (e: MouseEvent) => {
+            handleMouse(e, true);
+          },
+          onMouseLeave: (e: MouseEvent) => {
+            handleMouse(e, false);
+          },
+        }
+      : "";
+  const renderChildren = () => {
+    const subMenuClasses = classNames("viking-submenu", {
+      "menu-opened": menuOpen,
+    });
+    const childrenComponent = Children.map(children, (child, i) => {
+      const childElement = child as FunctionComponentElement<MenuItemProps>;
+      const { displayName } = childElement.type;
+      if (displayName === "MenuItem") {
+        return cloneElement(childElement, {
+          index: `${index}-${i}`,
+        });
+      }
+    });
 
-const  func = (congfig:SubMenuProps) => {
-
-}
-func({index: '1', cc: 2} as SubMenuProps)
-
-
-    const hoverEvents = context.mode !=='vertical' ? {
-       onMouseEnter: (e: MouseEvent) => { handleMouse(e, true,) },
-       onMouseLeave: (e: MouseEvent) => { handleMouse(e, false) }
-    } : ''
-    const renderChildren = () => {
-        const subMenuClasses = classNames('viking-submenu', {
-            'menu-opened': menuOpen
-          })
-        const childrenComponent =  Children.map( children, (child, i) => {
-            const childElement = child as FunctionComponentElement<MenuItemProps>
-            const { displayName } = childElement.type
-            if (displayName === 'MenuItem') {
-                return cloneElement(childElement, {
-                    index: `${index}-${i}`
-                })
-            }
-        })
-
-        return (
-            <ul className={subMenuClasses}>
-                { childrenComponent }
-            </ul>
-        )
-    }
-
-    
     return (
-        <li key={index} className={classes} style={style} { ...hoverEvents } >
-            <div className={titleClasses}  { ...clickEvents }> { title } </div>
-              { renderChildren() }
-        </li>
-    )
-}
+      <Transition in={menuOpen} timeout={300} animation="zoom-in-top">
+        <ul className={subMenuClasses}>{childrenComponent}</ul>
+      </Transition>
+    );
+  };
 
-SubMenu.displayName = "SubMenu"
+  return (
+    <li key={index} className={classes} style={style} {...hoverEvents}>
+      <div className={titleClasses} {...clickEvents}>
+        {" "}
+        {title}{" "}
+      </div>
+      {renderChildren()}
+    </li>
+  );
+};
 
-export default SubMenu
+SubMenu.displayName = "SubMenu";
+
+export default SubMenu;
